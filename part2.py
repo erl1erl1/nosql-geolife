d2720b566b8d3fe9e20ed74f2834c2d786c02203
grooleivsgard <gro.oleivsgard@gmail.com>
2023-10-20 09:11:22 +0200

    All queries added

--- part2.py
+++ part2.py
@@ -181,41 +181,204 @@ class Part2:
             if activity != next_activity:
                 continue
 
-            lat1 = trackpoints_list[i]['lat']
-            lon1 = trackpoints_list[i]['lon']
-            lat2 = trackpoints_list[i + 1]['lat']
-            lon2 = trackpoints_list[i + 1]['lon']
+            lat_1 = trackpoints_list[i]['lat']
+            lon_1 = trackpoints_list[i]['lon']
+            lat_2 = trackpoints_list[i + 1]['lat']
+            lon_2 = trackpoints_list[i + 1]['lon']
 
             # Calculate distance between the two points with haversine
-            distance_in_km += haversine((lat1, lon1), (lat2, lon2))
+            distance_in_km += haversine((lat_1, lon_1), (lat_2, lon_2))
 
         print("Query 7 - the total distance (in km) walked in 2008, by user with id = 112:", "\n")
         print(f"{distance_in_km:.2f}")
 
     """
-    8. Findthetop20userswhohavegainedthemostaltitudemeters.
+    8. Find the top 20 users who have gained the most altitude meters.
         ○ Output should be a field with (id, total meters gained per user).
         ○ Remember that some altitude-values are invalid
         ○ Tip: ∑ (𝑡𝑝 𝑛. 𝑎𝑙𝑡𝑖𝑡𝑢𝑑𝑒 − 𝑡𝑝 𝑛−1. 𝑎𝑙𝑡𝑖𝑡𝑢𝑑𝑒), 𝑡𝑝 𝑛. 𝑎𝑙𝑡𝑖𝑡𝑢𝑑𝑒 > 𝑡𝑝 𝑛−1. 𝑎𝑙𝑡𝑖𝑡𝑢𝑑𝑒
     """
 
+    def top_20_users_with_most_altitude_meters(self):
+        pipeline = {
+            '_id': False,
+            'user_id': True,
+            'activity_id': True,
+            'altitude': True
+        }
+        result = self.db.Trackpoint.find({}, pipeline)
+
+        trackpoint_alt = list(result)
+
+        user_alt = dict()
+
+        # Calculate gained altitude for every user
+        for index in range(len(trackpoint_alt)):
+            # Break if end of trackpoints
+            if index == len(trackpoint_alt) - 1:
+                break
+
+            user_id = trackpoint_alt[index]['user_id']
+            activity_id_1 = trackpoint_alt[index]['activity_id']
+            activity_id_2 = trackpoint_alt[index + 1]['activity_id']
+
+            # Only calculate altitudes for trackpoints within the same activity
+            if activity_id_1 != activity_id_2:
+                continue
+
+            # Initialize dictionary
+            if user_id not in user_alt:
+                user_alt[user_id] = 0
+
+            alt_1 = trackpoint_alt[index]['altitude']
+            alt_2 = trackpoint_alt[index + 1]['altitude']
+
+            # Only include valid altitudes
+            if not alt_1 or not alt_2:
+                continue
+
+            delta_alt = alt_2 - alt_1
+            user_alt[user_id] += delta_alt
+
+        # Sorting dictionary
+        user_alt_array = sorted(
+            user_alt.items(), key=lambda x: x[1], reverse=True)
+
+        results = []
+
+        for i, (user_id, alt) in enumerate(user_alt_array[:20]):
+            results.append([i + 1, user_id, round(alt)])
+
+        print(f"Query 8 - Top 20 users who have gained the most altitude meters:")
+        print(results)
+
     """
-    9. Findalluserswhohaveinvalidactivities,andthenumberofinvalidactivities per user
+    9. Find all users who have invalid activities,and the number of invalid activities per user
     ○ An invalid activity is defined as an activity with consecutive trackpoints where the timestamps deviate with at least 5 minutes.
     """
+
+    def users_with_invalid_activities(self):
+        pipeline = {
+            '_id': False,
+            'user_id': True,
+            'activity_id': True,
+            'date_time': True
+        }
+
+        result = self.db.TrackPoint.find({}, pipeline)
+        trackpoints = list(result)
+        invalid_user_activities = dict()
+
+        # Find invalid trackpoints
+        for index in range(len(trackpoints)):
+            if index == len(trackpoints) - 1:
+                break
+            user_id = trackpoints[index]['user_id']
+            activity_id_1 = trackpoints[index]['activity_id']
+            activity_id_2 = trackpoints[index + 1]['activity_id']
+
+            # Only compare trackpoints within the same activity
+            if activity_id_1 != activity_id_2:
+                continue
+
+            # Initialize dictionary
+            if user_id not in invalid_user_activities:
+                invalid_user_activities[user_id] = set()
+
+            date_time_1 = trackpoints[index]['date_time']
+            date_time_2 = trackpoints[index + 1]['date_time']
+
+            delta_date_time = date_time_2 - date_time_1
+            if delta_date_time.seconds > 60 * 5:
+                invalid_user_activities[user_id].add(activity_id_1)
+
+        # Sort dictionary
+        invalid_activities = sorted(invalid_user_activities.items())
+
+        results = []
+
+        for user_id, activities in invalid_activities:
+            results.append([user_id, len(activities)])
+
+        print("Query 9 - Users with invalid activities and the number of invalid activities:")
+        print(results)
 
     """
     10.Find the users who have tracked an activity in the Forbidden City of Beijing. ○ In this question you can consider the Forbidden City to have
     coordinates that correspond to: lat 39.916, lon 116.397.
     """
 
+    def users_with_activity_in_beijing(self):
+        pipeline = [{'$match': {
+            'lat': {
+                '$gte': 39.916,
+                '$lte': 39.917},
+            'lon': {
+                '$gte': 116.397,
+                '$lte': 116.398
+            }
+        }
+        }, {'$group': {
+            '_id': '$user_id',
+        }}]
+
+        result = self.db.TrackPoint.aggregate(pipeline)
+
+        results = []
+
+        for row in list(result):
+            results.append(f"User {row['_id']} has trackpoints in the forbidden city\n")
+
+        print("Query 10 - Users with tracked activity in the forbidden city Beijing:")
+        print(results)
+
     """
-    11.Find all users who have registered transportation_mode and their most used
-    transportation_mode.
-    ○ The answer should be on format (user_id,
-    most_used_transportation_mode) sorted on user_id.
+    11.Find all users who have registered transportation_mode and their most used transportation_mode.
+    ○ The answer should be on format (user_id, most_used_transportation_mode) sorted on user_id.
     ○ Some users may have the same number of activities tagged with e.g.
     walk and car. In this case it is up to you to decide which transportation
     mode to include in your answer (choose one).
     ○ Do not count the rows where the mode is null.
     """
+
+    def user_transportation_mode(self):
+        pipeline = {
+            'transportation_mode': {
+                '$ne': ''
+            }
+        }, {
+            '_id': False,
+            'user_id': True,
+            'transportation_mode': True
+        }
+        result = self.db.Activity.find(pipeline)
+
+        activities = list(result)
+        user_mode = dict()
+
+        for activity in activities:
+            user_id = activity['user_id']
+            transportation_mode = activity['transportation_mode']
+
+            # Initialize user_mode dictionary
+            if user_id not in user_mode:
+                user_mode[user_id] = dict()
+
+            # Initialize transportation_mode dictionary
+            if transportation_mode not in user_mode[user_id]:
+                user_mode[user_id][transportation_mode] = 0
+
+            # Count each transportation mode
+            user_mode[user_id][transportation_mode] += 1
+
+        # Sort the dictionary
+        user_mode_sorted = sorted(user_mode.items())
+
+        results = []
+
+        for user_id, transportation_modes in user_mode_sorted:
+            transportation_mode = max(transportation_modes, key=transportation_modes.get)
+            results.append([user_id, transportation_mode, transportation_modes[transportation_mode]])
+
+        print("Query 11 - Users with transportation mode registered and their most used transportation mode:")
+        print(results)
